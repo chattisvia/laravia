fdd36326e29ee0c5bad6f581ad045e67bc3331e1
Chatam Disty Fashanjaya <c.disvia@gmail.com>
2024-11-30 20:44:21 +0700

    Creating single-post Co-authored-by: chattischad <chattis@tensi.org>

---
+++ resources/views/post.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <article class="py-8 max-w-screen-md">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900 hover:text-blue-500">{{ $post['title'] }}
+        </h2>
+        <div class="text-base text-gray-500">
+            <a href="#">{{ $post['author'] }}</a> | 1 Januari 2024
+        </div>
+        <p class="my-4 font-light">{{ $post['body'] }}</p>
+        <a href="/posts" class="font-medium hover:underline hover:text-blue-500">&laquo; Back to blog</a>
+    </article>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,6 +15,7 @@ Route::get('/posts', function () {
     return view('posts', ['title' => 'Blogku', 'posts' => [
         [
             'id' => 1,
+            'slug' => 'judul-artikel-1',
             'title' => 'Judul Artikel 1',
             'author' => 'Chatam D. Fasha',
             'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur dolores, tenetur, porro delectus ea
@@ -23,6 +25,7 @@ Route::get('/posts', function () {
         ],
         [
             'id' => 2,
+            'slug' => 'judul-artikel-2',
             'title' => 'Judul Artikel 2',
             'author' => 'Chatam D. Fasha',
             'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur dolores, tenetur, porro delectus ea
@@ -32,6 +35,7 @@ Route::get('/posts', function () {
         ],
         [
             'id' => 3,
+            'slug' => 'judul-artikel-3',
             'title' => 'Judul Artikel 3',
             'author' => 'Chatam D. Fasha',
             'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur dolores, tenetur, porro delectus ea
@@ -41,6 +45,7 @@ Route::get('/posts', function () {
         ],
         [
             'id' => 4,
+            'slug' => 'judul-artikel-4',
             'title' => 'Judul Artikel 4',
             'author' => 'Chatam D. Fasha',
             'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur dolores, tenetur, porro delectus ea
@@ -49,6 +54,57 @@ Route::get('/posts', function () {
             Eum dicta laborum cum reiciendis tempora architecto blanditiis molestias libero illo.'
         ],
     ]]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Chatam D. Fasha',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur dolores, tenetur, porro delectus ea
+            dolorum cum harum sunt rerum iste possimus debitis eaque quia minus quas? Similique numquam corrupti
+            voluptate!
+            Eum dicta laborum cum reiciendis tempora architecto blanditiis molestias libero illo.'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Chatam D. Fasha',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur dolores, tenetur, porro delectus ea
+            dolorum cum harum sunt rerum iste possimus debitis eaque quia minus quas? Similique numquam corrupti
+            voluptate!
+            Eum dicta laborum cum reiciendis tempora architecto blanditiis molestias libero illo.'
+        ],
+        [
+            'id' => 3,
+            'slug' => 'judul-artikel-3',
+            'title' => 'Judul Artikel 3',
+            'author' => 'Chatam D. Fasha',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur dolores, tenetur, porro delectus ea
+            dolorum cum harum sunt rerum iste possimus debitis eaque quia minus quas? Similique numquam corrupti
+            voluptate!
+            Eum dicta laborum cum reiciendis tempora architecto blanditiis molestias libero illo.'
+        ],
+        [
+            'id' => 4,
+            'slug' => 'judul-artikel-4',
+            'title' => 'Judul Artikel 4',
+            'author' => 'Chatam D. Fasha',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur dolores, tenetur, porro delectus ea
+                dolorum cum harum sunt rerum iste possimus debitis eaque quia minus quas? Similique numquam corrupti
+                voluptate!
+                Eum dicta laborum cum reiciendis tempora architecto blanditiis molestias libero illo.'
+        ],
+
+    ];
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
