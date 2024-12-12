f488ccf88dfda23cc4672abc25354c8c1dcbeec1
Chatam Disty Fashanjaya <c.disvia@gmail.com>
2024-12-12 20:16:46 +0700

    Relasi Eloquent Co-authored-by: chattischad <chattis@tensi.org>

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -17,10 +18,13 @@ Route::get('/posts', function () {
 });
 
 Route::get('/posts/{post:slug}', function (Post $post) {
-    
     // $post = Post::find($id);
-
     return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+Route::get('/authors/{user}', function (User $user) {
+    // $post = Post::find($id);
+    return view('posts', ['title' => 'Articles by '. $user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/contact', function () {
